679a04b096d8d3d33236cd9492578b52a1988529
mks_dev <maximtimofeev.dev@gmail.com>
2024-03-11 10:51:20 +0300

    chanches from master for new func

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def lasy_f(a, b):
-    'blablablaa'
+    'blablabla.'
 
     return a + b
 
 
+def new():
+    print('qwer')
+    print('qwer')
+    print('qwer')
+
 def my_future_f():
-    """makes some things"""
+    """makes some things!"""
     size = 44
     price = 19
     return size + price
